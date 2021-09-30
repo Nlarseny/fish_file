540f3dc3e596846c57612312fcbd7abceb04b7c2
Nlarseny <larsen.d.nathan@gmail.com>
2021-09-30 17:34:02 -0600

    fixed hard code set up

--- fishypipe.py
+++ fishypipe.py
@@ -1,13 +1,29 @@
 # A fishy Script
 # example of how to run fastqc "./fastqc somefile.txt"
 
+# This allows us to run terminal commands
 import subprocess
+import sys, getopt
 
-fastqc_process = subprocess.run(["../FastQC/fastqc", "../rockfish_data/S_ciliatus_1003050_R1.fastq"])
+def main(argv):
+    opts, args = getopt.getopt(argv, "hi:o:",["ifile=","ofile="])
 
-# piping example
-# ps = subprocess.Popen(('ps', '-A'), stdout=subprocess.PIPE)
-# output = subprocess.check_output(('grep', 'process_name'), stdin=ps.stdout)
-# ps.wait()
+    # The .html files are just a way to see if the sequences are of good quality.
+    # The .html files are not used for any additional analyses.
+    # FIXED: hardcode issue (python3 fishypipe.py ../FastQC/fastqc ../rockfish_data/S_ciliatus_1003050_R1.fastq)
+    fastqc_process = subprocess.run([args[0], args[1]])
 
-print("100% complete")
+    # NOTE: I'm thinking we create an option to show the quality via html and then
+    # the user can choose to continue or not
+
+    subprocess.run(["open", "../rockfish_data/S_ciliatus_1003050_R1.html"])
+
+    # piping example
+    # ps = subprocess.Popen(('ps', '-A'), stdout=subprocess.PIPE)
+    # output = subprocess.check_output(('grep', 'process_name'), stdin=ps.stdout)
+    # ps.wait()
+
+    print("100% complete")
+
+if __name__ == "__main__":
+   main(sys.argv[1:])

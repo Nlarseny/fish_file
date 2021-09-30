b251c8fa0e667b906000756f92d799fbc5dcb972
Nlarseny <larsen.d.nathan@gmail.com>
2021-09-29 22:04:05 -0600

    basic script for fastqc

--- fishypipe.py
+++ fishypipe.py
@@ -3,4 +3,11 @@
 
 import subprocess
 
-list_files = subprocess.run(["ls", "-l"])
+fastqc_process = subprocess.run(["../FastQC/fastqc", "../rockfish_data/S_ciliatus_1003050_R1.fastq"])
+
+# piping example
+# ps = subprocess.Popen(('ps', '-A'), stdout=subprocess.PIPE)
+# output = subprocess.check_output(('grep', 'process_name'), stdin=ps.stdout)
+# ps.wait()
+
+print("100% complete")

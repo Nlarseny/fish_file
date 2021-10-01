b3409ceb5340f2146c9daf665bac685c4422957c
Nlarseny <larsen.d.nathan@gmail.com>
2021-09-30 20:05:31 -0600

    added option to continue or quit

--- fishypipe.py
+++ fishypipe.py
@@ -9,15 +9,25 @@ def fastq_to_html(arg):
     num = arg.rfind('.')
     temp = arg[0 : num]
     html_arg = temp + "_fastqc.html"
-    
+
     return html_arg
 
+def continue_or_not():
+    print("FastQC finished, would you like to continue? (y/n)")
+    answer = input().lower()
+
+    if answer == 'y':
+        return 0
+    elif answer == 'n':
+        return -1
+    else:
+         return continue_or_not()
 
 
 def main(argv):
     opts, args = getopt.getopt(argv, "hi:o:",["ifile=","ofile="])
     fastq_html = fastq_to_html(args[1])
-    subprocess.run(["open", fastq_html])
+
     # The .html files are just a way to see if the sequences are of good quality.
     # The .html files are not used for any additional analyses.
     # FIXED: hardcode issue (python3 fishypipe.py ../FastQC/fastqc ../rockfish_data/S_ciliatus_1003050_R1.fastq)
@@ -26,6 +36,11 @@ def main(argv):
     # NOTE: I'm thinking we create an option to show the quality via html and then
     # the user can choose to continue or not
     subprocess.run(["open", fastq_html])
+
+
+    continue_option = continue_or_not()
+    if continue_option < 0:
+        quit()
 
     # piping example
     # ps = subprocess.Popen(('ps', '-A'), stdout=subprocess.PIPE)

188e181812857d6b396971c7a09b2a5ad904230e
Nlarseny <larsen.d.nathan@gmail.com>
2021-10-07 21:44:06 -0600

    added support for trim_galore, and a way to skip fastqc

--- fishypipe.py
+++ fishypipe.py
@@ -13,7 +13,7 @@ def fastq_to_html(arg):
     return html_arg
 
 def continue_or_not():
-    print("FastQC finished, would you like to continue? (y/n)")
+    print("(y/n)")
     answer = input().lower()
 
     if answer == 'y':
@@ -26,28 +26,39 @@ def continue_or_not():
 
 def main(argv):
     opts, args = getopt.getopt(argv, "hi:o:",["ifile=","ofile="])
-    fastq_html = fastq_to_html(args[1])
 
+    fastq_html = fastq_to_html(args[0])
     # The .html files are just a way to see if the sequences are of good quality.
     # The .html files are not used for any additional analyses.
-    # FIXED: hardcode issue (python3 fishypipe.py ../FastQC/fastqc ../rockfish_data/S_ciliatus_1003050_R1.fastq)
-    fastqc_process = subprocess.run([args[0], args[1]])
+    # edited command: (python3 fishypipe.py ../rockfish_data/S_ciliatus_1003050_R1.fastq)
 
-    # NOTE: I'm thinking we create an option to show the quality via html and then
-    # the user can choose to continue or not
-    subprocess.run(["open", fastq_html])
-
-
+    print("\nRun fastqc? Trim_galore will run no matter what.")
     continue_option = continue_or_not()
-    if continue_option < 0:
-        quit()
+    if continue_option == 0:
+        fastqc_process = subprocess.run(["fastqc", args[0]])
+        subprocess.run(["open", fastq_html])
+
+        print("FastQC finished, would you like to continue?")
+        continue_option = continue_or_not()
+        if continue_option < 0:
+            quit()
+
+    # continuing with program, on to trimgalore
+    print("\n===== RUNNING TRIMGALORE =====\n")
+    fastqc_process = subprocess.run(["trim_galore", args[0]])
+
+    # TRIMGALORE will give a file in the fish file folder
+    
+
+
+
 
     # piping example
     # ps = subprocess.Popen(('ps', '-A'), stdout=subprocess.PIPE)
     # output = subprocess.check_output(('grep', 'process_name'), stdin=ps.stdout)
     # ps.wait()
 
-    print("100% complete")
+    print("\n\n===== 100% complete =====")
 
 if __name__ == "__main__":
    main(sys.argv[1:])
